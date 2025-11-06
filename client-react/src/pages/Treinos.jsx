d53795e4b7c341529fdffec0ce080df4cf7c45f6
sharmuse <48999864+sharmuse@users.noreply.github.com>
2025-11-06 15:55:42 -0300

    Create Treinos.jsx

---
+++ client-react/src/pages/Treinos.jsx
@@ -0,0 +1,8 @@
+export default function Treinos() {
+  return (
+    <section>
+      <h1 className="text-2xl font-bold mb-2">🏋️‍♂️ Treinos</h1>
+      <p className="text-slate-300">Aqui entram as prescrições/rotinas.</p>
+    </section>
+  )
+}
