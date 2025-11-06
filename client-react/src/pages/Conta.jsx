e742dffad230823a78bee466ac4db9580e3445ae
sharmuse <48999864+sharmuse@users.noreply.github.com>
2025-11-06 15:57:00 -0300

    Create Conta.jsx

---
+++ client-react/src/pages/Conta.jsx
@@ -0,0 +1,8 @@
+export default function Conta() {
+  return (
+    <section>
+      <h1 className="text-2xl font-bold mb-2">⚙️ Conta</h1>
+      <p className="text-slate-300">Preferências do usuário.</p>
+    </section>
+  )
+}
