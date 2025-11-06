c8edeaaa5004a010b5b79495e361ee736066f50b
sharmuse <48999864+sharmuse@users.noreply.github.com>
2025-11-06 15:56:17 -0300

    Create Notificacoes.jsx

---
+++ client-react/src/pages/Notificacoes.jsx
@@ -0,0 +1,8 @@
+export default function Notificacoes() {
+  return (
+    <section>
+      <h1 className="text-2xl font-bold mb-2">🔔 Notificações</h1>
+      <p className="text-slate-300">Notificações para professor/aluno.</p>
+    </section>
+  )
+}
