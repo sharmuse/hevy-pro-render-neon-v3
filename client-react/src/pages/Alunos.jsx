a1dc9cf201fcf5e1254c99f333ece2c818db037a
sharmuse <48999864+sharmuse@users.noreply.github.com>
2025-11-06 15:54:53 -0300

    Create Alunos.jsx

---
+++ client-react/src/pages/Alunos.jsx
@@ -0,0 +1,8 @@
+export default function Alunos() {
+  return (
+    <section>
+      <h1 className="text-2xl font-bold mb-2">👥 Alunos</h1>
+      <p className="text-slate-300">Aqui entra a lista de alunos.</p>
+    </section>
+  )
+}
